6e8eead3ff339d66245a18c37dd4c49057c3a495
Amjad584 <Amjad.business584@gmail.com>
2023-11-01 18:12:48 +0300

    Added toggling effect to search bar on mid scale

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,14 @@
 
+// When the search icon is clicked it will show the search form --only on max-width of 768px
+searchForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+}
 
 
 // Making the navigation bar fixed
 window.onscroll = () => {
+    searchForm.classList.remove('active');
     if (window.scrollY > 80) {
         document.querySelector('.header .header-2').classList.add('active');
     } else {
